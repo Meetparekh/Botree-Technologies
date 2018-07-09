cfaba605b92e84e6dbe7472ab509018d4c52a488
Meetparekh <36527326+Meetparekh@users.noreply.github.com>
2018-07-09 19:44:21 +0530

    Update armstrong_numbers.py

--- Armstrong number/armstrong_numbers.py
+++ Armstrong number/armstrong_numbers.py
@@ -1,2 +1,15 @@
 def is_armstrong(number):
-    pass
+    a=len(str(number))
+    total=0
+    temp=number
+    while temp>0:
+        digit = temp % 10
+        total=total+digit**a
+        temp = temp // 10
+
+    if number == total:
+        return True
+
+    else:
+        return False
+
